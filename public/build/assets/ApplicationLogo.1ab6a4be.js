3f9b77aa0558d275500f596e85a40f41a4f0a612
Angelo Neto <netopvh@gmail.com>
2022-09-12 02:09:15 -0400

    att build 2

---
+++ public/build/assets/ApplicationLogo.1ab6a4be.js
@@ -0,0 +1,1 @@
+import{a as i}from"./app.060b6cec.js";function g({className:o}){return i("img",{src:"/img/logo.svg",className:o})}export{g as A};
--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -8,12 +8,12 @@ export default function NavLink({ href, active, children }) {
             href={href}
             className={
                 active
-                    ? 'mb-4 inline-flex items-center uppercase px-1 pt-1 border-b-2 border-black text-md font-medium leading-5 text-gray-900 focus:outline-none focus:border-black transition duration-150 ease-in-out'
-                    : 'mb-2 inline-flex items-center uppercase px-1 pt-1 border-b-2 border-transparent text-md font-medium leading-5 text-gray-500 hover:text-black hover:border-black focus:outline-none focus:text-black focus:border-black transition duration-150 ease-in-out'
+                    ? 'mb-4 inline-flex items-center uppercase px-1 pt-1 text-md font-medium leading-5 text-gray-900 focus:outline-none transition duration-150 ease-in-out'
+                    : 'mb-2 inline-flex items-center uppercase px-1 pt-1 text-md font-medium leading-5 text-gray-500 hover:text-black focus:outline-none focus:text-black transition duration-150 ease-in-out'
             }
         >
             {children}
-            <FaAngleDoubleRight className="w-5 h-5 inline-block" />
+            <FaAngleDoubleRight className="w-5 h-5 inline-block ml-4" />
         </Link>
     );
 }
